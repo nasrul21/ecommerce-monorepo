d8080cf09a67138305fdf5171c663281d2ce4955
Nasrul Faizin <faizinnasrul21@gmail.com>
2025-05-29 13:59:16 +0700

    init project

---
+++ apps/db-migrations/knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ apps/db-migrations/migrations/20250529063553_create_table_test.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  return await knex.raw("CREATE TABLE tests")
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  return await knex.raw("DROP TABLE tests")
+};
